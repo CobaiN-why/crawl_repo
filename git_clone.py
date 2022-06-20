0c247456dc538eb16be0197d368936ae1f4e18c7
CobaiN-why <717898675@qq.com>
2022-06-20 16:45:59 +0800

    update script

--- git_clone.py
+++ git_clone.py
@@ -3,6 +3,8 @@ import git
 import os
 from datetime import datetime
 
+from nbformat import write
+
 
 class Progress(git.remote.RemoteProgress):
     def update(self, op_code, cur_count, max_count=None, message=''):
@@ -11,32 +13,31 @@ class Progress(git.remote.RemoteProgress):
             print("OK")
 
 
-global CSHARP_REPO_LIST
+global PYTHON_REPO_LIST
 global JAVA_REPO_LIST
-global CSHARP_DIR
+global PYTHON_DIR
 global JAVA_DIR
-CSHARP_LIST_PATH = "/mnt/qiuyuan/Data/top_1000_csharp/top_1000_csharp_list 202012231701.txt"
-JAVA_LIST_PATH = "/mnt/qiuyuan/Data/top_1000_java/top_1000_java_list 202012231653.txt"
-CSHARP_LOG_PATH = "/mnt/qiuyuan/Data/top_1000_csharp/top_1000_charp_list_log.txt"
-JAVA_LOG_PATH = "/mnt/qiuyuan/Data/top_1000_java/top_1000_java_list_log.txt"
+PYTHON_LIST_PATH = "./top_1000_python_list.txt"
+JAVA_LIST_PATH = "./top_1000_java_list.txt"
+PYTHON_LOG_PATH = "F:\\top_1000_repo\\top_1000_python\\top_1000_python_list_log.txt"
+JAVA_LOG_PATH = "F:\\top_1000_repo\\top_1000_java\\top_1000_java_list_log.txt"
 
-with open(CSHARP_LIST_PATH) as f:
-    csharp_list = list(f.readlines())
+with open(PYTHON_LIST_PATH) as f:
+    python_list = list(f.readlines())
 
 with open(JAVA_LIST_PATH) as f:
     java_list = list(f.readlines())
 
-CSHARP_REPO_LIST = [url.replace("https:", "git:").strip()
-                    for url in csharp_list]
-JAVA_REPO_LIST = [url.replace("https:", "git:").strip() for url in java_list]
+PYTHON_REPO_LIST = [url.strip() for url in python_list]
+JAVA_REPO_LIST = [url.strip() for url in java_list]
 
-CSHARP_DIR = "/mnt/qiuyuan/Data/top_1000_csharp/projects"
-JAVA_DIR = "/mnt/qiuyuan/Data/top_1000_java/projects"
+PYTHON_DIR = "F:\\top_1000_repo\\top_1000_python\\projects"
+JAVA_DIR = "F:\\top_1000_repo\\top_1000_java\\projects"
 REPO_DIR = os.path.join(JAVA_DIR, "projects")
 
-# check global variable CSHARP_REPO_LIST
-assert len(CSHARP_REPO_LIST) == len(list(set(CSHARP_REPO_LIST)))
-assert len(CSHARP_REPO_LIST) >= 1020
+# check global variable PYTHON_REPO_LIST
+assert len(PYTHON_REPO_LIST) == len(list(set(PYTHON_REPO_LIST)))
+assert len(PYTHON_REPO_LIST) >= 1020
 
 # check global variable JAVA_REPO_LIST
 assert len(JAVA_REPO_LIST) == len(list(set(JAVA_REPO_LIST)))
@@ -45,11 +46,10 @@ assert len(JAVA_REPO_LIST) >= 1020
 
 def test_clone_one():
     # clone_one(JAVA_REPO_LIST[0], JAVA_DIR)
-    clone_one(CSHARP_REPO_LIST[0], CSHARP_DIR)
+    clone_one(PYTHON_REPO_LIST[0], PYTHON_DIR)
 
 
 def clone_one(git_url, to_dir, rank=None):
-    assert git_url.startswith("git://"), git_url
     assert not git_url.endswith("\n")
 
     repo_name = git_url.split("/")[-1]
@@ -69,30 +69,68 @@ def clone_one(git_url, to_dir, rank=None):
     return git_url, pull_info
 
 
+def read_file(path):
+    """load lines from a file"""
+    sents = []
+    with open(path, 'r') as f:
+        for line in f:
+            sents.append(str(line.strip()))
+    return sents
+
+
+def write_file(filename, data):
+    with open(filename, 'w') as f:
+        for i in data:
+            f.write(str(i).strip() + '\n')
+
+
+def check_already(logs, repo):
+    flag = 1
+    for line in logs:
+        if repo in line:
+            flag = 0
+    return flag
+
+
 def clone_list(repo_list, to_dir, log_file):
-    with open(log_file, "w") as f:
+    with open(log_file, "a") as f:
         start = datetime.now().strftime("Time: %Y-%m-%d %H:%M\n")
         f.write(start)
+    logs = read_file(log_file)
+    failed_list = []
     for index, repo in enumerate(repo_list):
         # e.g., top0001, top0002, top0003 etc.
         rank = "top%s_" % str(index+1).rjust(4, "0")
-        git_url, pull_info = clone_one(repo, to_dir, rank)
-        message = "%s || %s\n" % (git_url, pull_info)
-        now = datetime.now().strftime("Time: %Y-%m-%d %H:%M\n")
-        with open(log_file, "a+") as f:
-            f.write(message)
-            f.write(now)
+        
+        if check_already(logs, repo) and index < 1000:
+            try:
+                git_url, pull_info = clone_one(repo, to_dir, rank)
+                message = "%s || %s\n" % (git_url, pull_info)
+                now = datetime.now().strftime("Time: %Y-%m-%d %H:%M\n")
+                with open(log_file, "a+") as f:
+                    f.write(message)
+                    f.write(now)
+            except Exception as e:
+                failed_list.append(repo)
+                with open(log_file, "a+") as f:
+                    f.write("%s failed!!!!!!" % (index+1))
+                    f.write(now)
+        else:
+            continue
+    
+    failed_file = log_file[:-7] + "failed.txt"
+    write_file(failed_file, failed_list)
 
 
 def main():
     # test_clone_one()
 
-    # check global variable CSHARP_DIR
-    print("REPO directory:" + CSHARP_DIR)
-    for _, dir, file in os.walk(CSHARP_DIR):
-        assert dir == [], "repo dir should be empty"
-        assert file == [], "repo dir should be empty"
-    clone_list(CSHARP_REPO_LIST, CSHARP_DIR, CSHARP_LOG_PATH)
+    # check global variable PYTHON_DIR
+    print("REPO directory:" + PYTHON_DIR)
+    # for _, dir, file in os.walk(PYTHON_DIR):
+    #     assert dir == [], "repo dir should be empty"
+    #     assert file == [], "repo dir should be empty"
+    clone_list(PYTHON_REPO_LIST, PYTHON_DIR, PYTHON_LOG_PATH)
 
     # check global variable JAVA_DIR
     # print("REPO directory:" + JAVA_DIR)
--- github_search.py
+++ github_search.py
@@ -4,102 +4,14 @@ import os
 
 COLLECTION_DIR = "./"
 
+ACCESS_USERNAME = '7178986756@qq.com'
+ACCESS_PWD = "Wang717898675"
+
 with open(os.path.join(COLLECTION_DIR, "github_token.txt")) as f:
     token = f.read()
 
 g = Github(token)
-
-def search_top1000_csharp():
-    repository_path_csharp = os.path.join(COLLECTION_DIR, "top_1000_csharp_list.txt")
-    star_path_csharp = os.path.join(COLLECTION_DIR, "top_1000_stars_csharp.txt")
-    repo_dict = {}
-
-    # 因为网络问题，连续跑四次，查漏补缺
-    csharp_repositories = g.search_repositories(query='language:c#', sort='stars', order='desc')
-    for repo in csharp_repositories:
-        print(repo.clone_url)
-        repo_dict[repo.clone_url] = repo.stargazers_count
-        sleep(0.03)
-
-    csharp_repositories = g.search_repositories(query='language:c#', sort='stars', order='desc')
-    for repo in csharp_repositories:
-        print(repo.clone_url)
-        repo_dict[repo.clone_url] = repo.stargazers_count
-        sleep(0.03)
-
-    csharp_repositories = g.search_repositories(query='language:c#', sort='stars', order='desc')
-    for repo in csharp_repositories:
-        print(repo.clone_url)
-        repo_dict[repo.clone_url] = repo.stargazers_count
-        sleep(0.03)
-
-
-    csharp_repositories = g.search_repositories(query='language:c#', sort='stars', order='desc')
-    for repo in csharp_repositories:
-        print(repo.clone_url)
-        repo_dict[repo.clone_url] = repo.stargazers_count
-        sleep(0.03)
-
-    sorted_repo = sorted(repo_dict.items(), key=lambda x: x[1], reverse=True)
-    print(len(sorted_repo))
-
-    with open(repository_path_csharp, "w") as f:
-        with open(star_path_csharp, "w") as star:
-            for clone_url, stars in sorted_repo:
-                print(clone_url)
-                f.write(clone_url)
-                f.write("\n")
-
-                star.write(str(stars))
-                star.write("\n")
-    
-    assert len(sorted_repo) > 1000, "Not enough dirs, it should be larger than 1000 (sometimes 1020)"
-
-def search_top1000_java():
-    repository_path_java = os.path.join(COLLECTION_DIR, "top_1000_java_list.txt")
-    star_path_java = os.path.join(COLLECTION_DIR, "top_1000_stars_java.txt")
-    repo_dict = {}
-    
-    # 因为网络问题，连续跑四次，查漏补缺
-    java_repositories = g.search_repositories(query='language:java', sort='stars', order='desc')
-    for repo in java_repositories:
-        print(repo.clone_url)
-        repo_dict[repo.clone_url] = repo.stargazers_count
-        sleep(0.03)
-
-    java_repositories = g.search_repositories(query='language:java', sort='stars', order='desc')
-    for repo in java_repositories:
-        print(repo.clone_url)
-        repo_dict[repo.clone_url] = repo.stargazers_count
-        sleep(0.03)
-
-    java_repositories = g.search_repositories(query='language:java', sort='stars', order='desc')
-    for repo in java_repositories:
-        print(repo.clone_url)
-        repo_dict[repo.clone_url] = repo.stargazers_count
-        sleep(0.03)
-
-    java_repositories = g.search_repositories(query='language:java', sort='stars', order='desc')
-    for repo in java_repositories:
-        print(repo.clone_url)
-        repo_dict[repo.clone_url] = repo.stargazers_count
-        sleep(0.03)
-
-    sorted_repo = sorted(repo_dict.items(), key=lambda x: x[1], reverse=True)
-    print(len(sorted_repo))
-
-    with open(repository_path_java, "w") as f:
-        with open(star_path_java, "w") as star:
-            for clone_url, stars in sorted_repo:
-                print(clone_url)
-                f.write(clone_url)
-                f.write("\n")
-
-                star.write(str(stars))
-                star.write("\n")
-
-    
-    assert len(sorted_repo) > 1000, "Not enough dirs, it should be larger than 1000 (sometimes 1020)"
+# g = Github(ACCESS_USERNAME, ACCESS_PWD)
 
 
 def search_top1000_type(lang_type):
@@ -112,25 +24,41 @@ def search_top1000_type(lang_type):
     for repo in lang_repositories:
         print(repo.clone_url)
         repo_dict[repo.clone_url] = repo.stargazers_count
-        sleep(0.03)
+        sleep(1)
+        if len(repo_dict) % 100 == 0:
+            print("===========================")
+        if len(repo_dict) > 1500:
+            break
 
     lang_repositories = g.search_repositories(query='language:'+lang_type, sort='stars', order='desc')
     for repo in lang_repositories:
         print(repo.clone_url)
         repo_dict[repo.clone_url] = repo.stargazers_count
-        sleep(0.03)
+        sleep(1)
+        if len(repo_dict) % 100 == 0:
+            print("===========================")
+        if len(repo_dict) > 1500:
+            break
 
     lang_repositories = g.search_repositories(query='language:'+lang_type, sort='stars', order='desc')
     for repo in lang_repositories:
         print(repo.clone_url)
         repo_dict[repo.clone_url] = repo.stargazers_count
-        sleep(0.03)
+        sleep(1)
+        if len(repo_dict) % 100 == 0:
+            print("===========================")
+        if len(repo_dict) > 1500:
+            break
 
     lang_repositories = g.search_repositories(query='language:'+lang_type, sort='stars', order='desc')
     for repo in lang_repositories:
         print(repo.clone_url)
         repo_dict[repo.clone_url] = repo.stargazers_count
-        sleep(0.03)
+        sleep(1)
+        if len(repo_dict) % 100 == 0:
+            print("===========================")
+        if len(repo_dict) > 1500:
+            break
 
     sorted_repo = sorted(repo_dict.items(), key=lambda x: x[1], reverse=True)
     print(len(sorted_repo))
